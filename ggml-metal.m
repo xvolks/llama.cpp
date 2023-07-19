2c6659e58db9371e592c7cfb896ff422a3705d4f
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2023-07-20 01:00:27 +0300

    Faster Q4_1 on Metal
    20.7 ms/t -> 19.7 ms/t

--- ggml-metal.m
+++ ggml-metal.m
@@ -671,8 +671,8 @@ void ggml_metal_graph_compute(
                                             GGML_ASSERT(ne02 == 1);
                                             GGML_ASSERT(ne12 == 1);
 
-                                            nth0 = 8;
-                                            nth1 = 8;
+                                            nth0 = 2;
+                                            nth1 = 32;
                                             [encoder setComputePipelineState:ctx->pipeline_mul_mat_q4_1_f32];
                                         } break;
                                     case GGML_TYPE_Q2_K:
